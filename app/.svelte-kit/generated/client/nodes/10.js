ad30b44cc09591367af0590ea87df88b2e13a594
neoan <stefan.roehrl@gmail.com>
2026-01-28 11:16:29 -0500

    feat: Add administration interface and API for managing plans, including SvelteKit generated files.

--- app/.svelte-kit/generated/client/nodes/10.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/(protected)/administration/members/+page.svelte";
--- app/.svelte-kit/generated/client/nodes/16.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/adoption-info/+page.svelte";
